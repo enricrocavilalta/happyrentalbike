c7f9870401f03c1ba669f07ca0c3a6d9a4f4dd3f
enricroca <enricrocavilalta@gmail.com>
2025-05-13 14:59:36 +0200

    recovered urban-bike.html

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const bikesRouter = require('./routes/bikes'); // Importa las rutas de bikes
 const toursRouter = require('./routes/tours'); // Importa las rutas de tours
 const app = express();
-
+const path = require('path');
 // Middleware para parsear el cuerpo de las peticiones como JSON
 app.use(bodyParser.json());
 
@@ -14,15 +14,36 @@ app.use('/tours', toursRouter);
 
 app.use(express.static('public'));
 
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
 // Arrancar el servidor
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
 
+// Set the views folder where EJS templates are located
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files (like CSS, JS, images)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to render the homepage
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.render('index'); // This will render views/index.ejs
 });
 
+app.get('/:page', (req, res) => {
+  const page = req.params.page;
+
+  res.render(page, (err, html) => {
+    if (err) {
+      res.status(404).send('Página no encontrada');
+    } else {
+      res.send(html);
+    }
+  });
+});
 
 
